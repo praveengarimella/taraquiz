6eb9999160b063676f1402adeca09114485cabfa
vedavyas <vedavyas.yeleswarapu@gmail.com>
2015-04-01 16:11:21 +0530

    displays basic layout of the page

--- GeneralCompetencyTest/app.js
+++ GeneralCompetencyTest/app.js
@@ -27,12 +27,31 @@ $(function() {
 
 		getQuizTitle : function() {
 			return quizModel.data.name;
+		},
+
+		getQuizSubsection : function() {
+			var currentSection = 0;
+			var currentSubsection = 0;
+			return quizModel.data.section[currentSection].name + ' - ' + quizModel.data.section[currentSection].subsection[currentSubsection].name;
+		},
+
+		ProgressButtonsBar : function() {
+			for (var i=1; i<=30; i++){
+				$("#buttonBar").append('<button type="button" id="'+i+'" class="btn btn-default btn-xs">'+i+'</button>&nbsp;');
+				$("#"+i).addClass('disabled');
+			}
+		},
+		initStartButton : function() {
+			$("#contentbox").html(
+				"<button id='starttest' class='btn btn-success'>Click to Start Test</button>"
+			);
 		}
 	};
 
 	var testProgressView = {
 
 		title : $(".title"),
+		subsection : $("#h4"),
 
 		init : function() {
 			// initialize the test progress view
@@ -41,21 +60,25 @@ $(function() {
 
 		render : function() {
 			// render the test progress view
-			console.log("inside render" + octopus.getQuizTitle());
 			this.title.html(octopus.getQuizTitle());
+			this.subsection.html(octopus.getQuizSubsection());
+			octopus.ProgressButtonsBar();
 		}
 	};
 
 	var questionView = {
+			
 		init : function() {
-			// initialize with a start button
-
-			// initialize the submit answer button
-
-			// initialize the continue button
+			this.render();
 		},
 
 		render : function() {
+			// initialize with a start button
+				octopus.initStartButton();
+			// initialize the submit answer button
+
+			// initialize the continue button
+
 			// dispaly the question using the question type and data template approach
 		}
 	};
